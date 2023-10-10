d35c6cea8a7038b872016dbf7d9248bac70e19af
Satyam Maurya <smauryachs44@gmail.com>
2023-10-10 16:40:18 +0530

    solved

---
+++ checkprime.js
@@ -0,0 +1,18 @@
+
+let gg= 12;
+
+let count=0;
+
+for(let i=1; i>=gg; i++){
+    if(gg%i==2){
+        count++
+    }
+
+}
+
+if (count == 2){
+    console.log("Yes, It is a prime number")
+}
+else{
+    console.log("No, It is not a prime number")
+}
